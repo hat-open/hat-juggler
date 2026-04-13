a82e7c97ccdc9519bd422d8e519b11f3b1cd6c84
bozo.kopic <bozo@kopic.xyz>
2026-04-14 00:26:58 +0200

    requirements update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -8,7 +8,6 @@ from hat.doit.js import (get_task_build_npm,
                          run_eslint)
 from hat.doit.py import (get_task_build_wheel,
                          get_task_run_pytest,
-                         get_task_create_pip_requirements,
                          run_flake8)
 
 
@@ -20,8 +19,7 @@ __all__ = ['task_clean_all',
            'task_build_ts',
            'task_check',
            'task_test',
-           'task_docs',
-           'task_pip_requirements']
+           'task_docs']
 
 
 build_dir = Path('build')
@@ -98,8 +96,3 @@ def task_docs():
                    dst_dir=build_docs_dir / 'py_api')
 
     return {'actions': [build]}
-
-
-def task_pip_requirements():
-    """Create pip requirements"""
-    return get_task_create_pip_requirements()
